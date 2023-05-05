0b82762e3cda44967e3b060adee6b049e532ba59
Thomas <79278097+thomassalcedo@users.noreply.github.com>
2023-05-05 13:40:57 +0200

    Update cémantix_helper.js

--- cémantix_helper.js
+++ cémantix_helper.js
@@ -1,15 +1,17 @@
 wordArray = [
-    "a", "à", "dans", "par", "pour", "en", "ver", "vert", "verre", "avec", "deux", "sans", "cent", "sang", "sous", "sur", "sûr", "lui",
-    "mais", "ou", "où", "est", "et", "est", "donc", "or", "hors", "ni", "car",
+    "à", "dans", "par", "pour", "en", "ver", "vert", "verre", "avec", "deux", "sans", "cent", "sang", "sous", "sur", "sûr", "lui",
+    "mais", "ou", "où", "et", "donc", "or", "hors", "ni", "car",
     "qui", "que", "quoi", "donc",
-    "mon", "ton", "son", "ma", "ta", "sa", "mes", "tes", "ses",
+    "mon", "ton", "son", "ma", "ta", "sa", "mes", "tes", "ses"
     "un", "une", "de", "des", "du", "au", "aux", "le" , "la", "les", "se",
     "e", "r", "t", "y", "p", "q", "qu", "d", "j", "l", "m", "c", "n", "s",
-    "je", "tu", "il", "elle", "nous", "vous", "ils", "elles",
+    "je", "tu", "il", "elle", "on", "nous", "vous", "ils", "elles",
     "lequel", "lesquels", "auquel", "auquels",
     "durant", "pendant", "avant", "après",
     "comment", "quand",
+    "avoir", "être",
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31",
+    "er", "ième", "ème",
     "i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix", "x", "xi", "xii", "xiii", "xiv", "xv", "xvi", "xvii", "xviii", "xix", "xx", "xxi"
 ];
 
